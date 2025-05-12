daba5fadef2beb6910045df979c1be7b4c13303f
roukayachelly <roukayachelly7@gmail.com>
2025-05-12 07:44:57 +0100

    feat: initialize Angular frontend application with core components and services

---
+++ frontend/src/app/components/permission-management/permission-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permission-management',
+  imports: [],
+  templateUrl: './permission-management.component.html',
+  styleUrl: './permission-management.component.css'
+})
+export class PermissionManagementComponent {
+
+}
