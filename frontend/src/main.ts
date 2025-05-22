eaaded794fac2f2029e7291b3c3bdd15aaf40d0e
roukayachelly <roukayachelly7@gmail.com>
2025-05-22 11:14:33 +0100

    feat: refactor main.ts to configure application providers and interceptors

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { routes } from './app/app.routes';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptorService } from './app/services/token-interceptor.service';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+
+
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+
+
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+  ]
+}).catch(err => console.error(err));
